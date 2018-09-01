07234f7b3e6f2e2281de142dc8c166e50a73688c
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-09-01 11:33:35 -0500

    secure stuff need to fix

--- database/client.js
+++ database/client.js
@@ -3,17 +3,13 @@ const pgp          = require('pg-promise')()
 const databaseName = process.env.NODE_ENV == 'test' ? 'movie_search_engine_test' : 'movie_search_engine'
 const host         = process.env.HEROKU == 'true' ? process.env.DATABASE_URL : 'localhost'
 
-
-
 const cn = {
     host: host,
     port: 5432,
     // database: `dry-wildwood-46109`,
-    // database:  process.env.HEROKU == 'true' ? null : databaseName,
+    database:  process.env.HEROKU == 'true' ? null : databaseName,
 }
 
 let db = pgp(cn);
-
-console.log(host)
 
 module.exports = db
