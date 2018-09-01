ce71fdf4e363f4e6512d273272b8b4fa0e871b4e
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-09-01 11:19:38 -0500

    working that remote sql

--- database/client.js
+++ database/client.js
@@ -3,12 +3,17 @@ const pgp          = require('pg-promise')()
 const databaseName = process.env.NODE_ENV == 'test' ? 'movie_search_engine_test' : 'movie_search_engine'
 const host         = process.env.HEROKU == 'true' ? process.env.DATABASE_URL : 'localhost'
 
+
+
 const cn = {
     host: host,
     port: 5432,
-    database: databaseName,
+    // database: `dry-wildwood-46109`,
+    // database:  process.env.HEROKU == 'true' ? null : databaseName,
 }
 
 let db = pgp(cn);
+
+console.log(host)
 
 module.exports = db
